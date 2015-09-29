25496eb92d73433c7c31345b4228d5782119263c
Zack McCauley <zack.mccauley@bsd7.org>
2015-09-29 10:13:12 -0600

    First Release. Not tested as of yet.

---
+++ MunkiSync.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# MunkiSync
+# Runs as a LaunchAgent and syncs everything from a master repo to a drive
+
+# Libraries
+import subprocess
+import os
+import logging
+from mount_shares_better import mount_share
+
+# Variables
+MASTER_MOUNT_PT="/Volumes/Munki"
+MASTER_MOUNT_IP = "smb://192.168.145.26/Munki"
+MUNKI_MASTER="/Volumes/Munki/munki_repo/"
+MUNKI_LOCAL="/Volumes/Munki HD/munki_repo"
+
+# Functions
+def UnmountServer():
+    if (os.path.isdir( MASTER_MOUNT_PT )):
+        unmount = subprocess.check_call(["/sbin/umount",MASTER_MOUNT_PT])
+
+def SyncEverything():
+    if (os.path.isdir( MUNKI_LOCAL )):
+        sync = subprocess.check_call(["/usr/local/bin/rsync", "--exclude=\".*\"", "-r", "--perms", "--times", "--progress", "--delete-after", "MUNKI_MASTER", "MUNKI_LOCAL"])
+    else:
+        print("%s is not available to sync too. ")
+        exit()
+
+def UpdatePermissions():
+    print("Updating Permissions.")
+    perms = subprocess.check_call(["/bin/chmod", "-R", "755", MUNKI_LOCAL])
+
+# Main
+def main():
+    if not(os.path.isdir( MASTER_MOUNT_PT )):
+        print("Ok to mount at %s".format(MASTER_MOUNT_PT))
+        if mount_share(MASTER_MOUNT_IP):
+            print("Mounted Munki Master (%s) at %s".format(MASTER_MOUNT_IP,MASTER_MOUNT_PT))
+    SyncEverything()
+    UpdatePermissions()
+    UnmountServer()
+
+main();
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # MunkiSync
+This is a conversion to python of a script I found/modified in bash that mounts a server and rsync's everything.
+It uses the following:
+from @pudquick aka frogor Crockett https://gist.github.com/pudquick/1362a8908be01e23041d
+from somewhere on StackOverflow (if I find the answer I will link) and modified for use:
+https://gist.github.com/WardsParadox/a3935a44a3b749761a1e
+
+A HUGE THANKS TO @pudquick for the help on macadmins.org (@frogor).
