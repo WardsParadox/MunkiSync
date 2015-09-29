38631a9315435b1b7a664d161fc793ca491f3f1e
Zack McCauley <zack.mccauley@bsd7.org>
2015-09-29 12:44:37 -0600

    Fixed .format issues and added better logic

--- MunkiSync.py
+++ MunkiSync.py
@@ -5,12 +5,11 @@
 # Libraries
 import subprocess
 import os
-import logging
 from mount_shares_better import mount_share
 
 # Variables
 MASTER_MOUNT_PT = "/Volumes/Munki"
-MASTER_MOUNT_IP = "smb://user:pass@serverip/Munki"
+MASTER_MOUNT_IP = "smb://username:password@ip/Munki"
 MUNKI_MASTER = "/Volumes/Munki/munki_repo/"
 MUNKI_LOCAL = "/Volumes/Munki HD/munki_repo"
 
@@ -26,20 +25,24 @@ def SyncEverything():
     if (os.path.isdir(MUNKI_LOCAL)):
         sync = subprocess.check_call(['/usr/local/bin/rsync', '--exclude=\".*\"', '-r', '--perms', '--times', '--progress', '--delete-after', MUNKI_MASTER, MUNKI_LOCAL])
     else:
-        print("%s is not available to sync too." % MUNKI_LOCAL)
+        print "{0} is not available to sync too.".format(MUNKI_LOCAL)
         exit()
 
 
 def UpdatePermissions():
-    print("Updating Permissions.")
+    print "Updating Permissions."
     perms = subprocess.check_call(["/bin/chmod", "-R", "755", MUNKI_LOCAL])
 
 # Main
 
 
 def main():
-    mount_share(MASTER_MOUNT_IP)
-    print("Mounted Munki Master (%s) at %s".format(MASTER_MOUNT_IP, MASTER_MOUNT_PT))
+    print "Mounting Share at {0} ".format(MASTER_MOUNT_PT)
+    if (os.path.isdir(MASTER_MOUNT_PT)):
+        print "Already Mounted Munki Master at {0}".format(MASTER_MOUNT_PT)
+    else:
+        mount_share(MASTER_MOUNT_IP)
+        print "Mounted Munki Master ({0}) at {1}".format(MASTER_MOUNT_IP, MASTER_MOUNT_PT)
     SyncEverything()
     UpdatePermissions()
     UnmountServer()
