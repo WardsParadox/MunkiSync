c62efeae594b26ddf742c3e3f4030c78a99a65c6
Zack McCauley <zack.mccauley@bsd7.org>
2015-09-29 13:05:33 -0600

    Fixed RSYNC binary location

--- MunkiSync.py
+++ MunkiSync.py
@@ -23,7 +23,7 @@ def UnmountServer():
 
 def SyncEverything():
     if (os.path.isdir(MUNKI_LOCAL)):
-        sync = subprocess.check_call(['/usr/local/bin/rsync', '--exclude=\".*\"', '-r', '--perms', '--times', '--progress', '--delete-after', MUNKI_MASTER, MUNKI_LOCAL])
+        sync = subprocess.check_call(['/usr/bin/rsync', '--exclude=\".*\"', '-r', '--perms', '--times', '--progress', '--delete-after', MUNKI_MASTER, MUNKI_LOCAL])
     else:
         print "{0} is not available to sync too.".format(MUNKI_LOCAL)
         exit()
